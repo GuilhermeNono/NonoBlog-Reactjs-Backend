565bad245d35e9ba042accc576b9fd696771d86c
Guilherme Nono <guilherme.nono45789@gmail.com>
2022-11-29 14:32:39 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const router = require('./routes/Router.js')
+require('dotenv').config()
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors({credentials:true, origin:process.env.ORIGIN}))
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+require("./config/db.js")
+
+app.use(router)
+
+
+app.listen(port, () => {
+    console.log(`Rodando na porta ${port}`)
+});
